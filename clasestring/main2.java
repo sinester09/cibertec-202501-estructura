2bfad7ed986e87542dc66d4325bcecb2e7ba2890
sinester09 <60792457+sinester09@users.noreply.github.com>
2025-05-31 14:21:09 -0500

    Create main2.java

---
+++ clasestring/main2.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    private static String contar_vocales(String palabra) {
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+        String palabra="ALUMNO";
+
+        int ultimo=palabra.length()-1;
+        int primero=0;
+        int mitad=(int) palabra.length()/2;
+
+            boolean igual= palabra.equals("alumno");
+
+
+//        System.out.println("ultimo: "+palabra.charAt(ultimo)+ " 1ro: "+palabra.charAt(primero)+" mitad"+palabra.charAt(mitad));
+
+
+            System.out.println(igual);
+    }
+}
